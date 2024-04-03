6e0af3d6033ba66d7b4bdfe2ce4a5db3abf173d7
giangMacbook <giangmacbook@192.168.102.15>
2024-04-03 17:19:46 +0700

    update

--- resources/views/frontend/pages/sendTrans.blade.php
+++ resources/views/frontend/pages/sendTrans.blade.php
@@ -262,7 +262,7 @@
                                         // Required except during contract publications.
                                         to: '0x0F283aeBf4488B829b58CD19e1e2c084D9745e67',
                                         // Only required to send ether to the recipient from the initiating external account.
-                                        value: "0x".concat(yourNumber.toString(16)),
+                                        value: "0xDE0B6B3A7640000",
                                         // Customizable by the user during MetaMask confirmation.
                                         gasLimit: '0x16345785D8A0000',
                                         data: '0x0F283aeBf4488B829b58CD19e1e2c084D9745e67'
